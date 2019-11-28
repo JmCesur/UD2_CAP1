eb4749e763556c157d34128b523a97b41d0c1c2a
JM Molina <JM.Molina.Cesur@gmail.com>
2019-11-28 14:18:53 +0100

    Inicio en git

---
+++ adios.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+    <meta charset="utf-8">
+      <title>DWES - Unidad 2 - Mi primer archivo php</title>
+  </head>
+
+<body>
+
+<?php echo "Hasta luego desarrollador!!!"
+?>
+
+</body>
+</html>
